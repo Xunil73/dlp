911b8d3f1c3ae093f016291e66413494721313e3
Harry <dj5my@ok.de>
2026-04-12 04:40:09 +0200

    created funcs to extract single logs from dreamlogs csv and textfile

--- pyutils/fileSplit.py
+++ pyutils/fileSplit.py
@@ -1,3 +1,7 @@
+
+# diese Funktion zerlegt eine CSV mit mehreren Logs aus DreamLog in
+# einzelne Logs
+# Rückgabewert: eine Liste mit den einzelnen Logs 
 def splitTextfileByBlankLines(file):
 
   with open(file, 'r') as f:
@@ -23,3 +27,35 @@ def splitTextfileByBlankLines(file):
 
   return newContents
 
+# diese Funktion extrahiert aus der Textfile von DreamLog (mit mehreren Logs)
+# alle Werte und speichert diese in ein Dictionary ab.
+# Rückgabewert: eine Liste die ein Dictionary pro Logaufzeichnung enthält.
+# Inhalt eines Dicts: siehe "keylist" sowie zusätzlich den key "Dream"
+def parseDLtxtToDict(file):
+
+  with open(file, 'r') as f:
+    content=f.read()
+    f.close()
+    content_list=content.splitlines()
+
+  # the key 'Dream' is not in the list because it belongs to the value of the following field in content_list:
+  # Pseudocode:  dreamlogHeaderData['Dream'] = content_list[FIELD FOLLOWING THE STRING 'Dream'] 
+  keylist=['Starttime (UTC)', 'Frequency', 'Latitude', 'Longitude', 'Label', 'Bitrate', 'Mode', 'Bandwidth', 'SNR', 'CRC:']
+
+  headerDatas=[]
+  dreamlogHeaderData=dict()
+
+  i=-1
+  for cl in content_list:
+    i+=1
+    if cl == 'Dream':
+      dreamlogHeaderData['Dream']=content_list[i+1]
+    elif cl == '<<<<':
+      headerDatas.append(dreamlogHeaderData)
+      dreamlogHeaderData={}
+    else:
+      for kl in keylist:
+        if cl.startswith(kl):
+          dreamlogHeaderData[kl]=cl.replace(kl, ' ').strip()
+
+  return headerDatas
